a79555dad47439223805ab309188e46478b0baed
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-01 13:44:07 +0100

    Fix: Remove trailing blank lines in band name generator

--- Day1/Projectday1.py
+++ Day1/Projectday1.py
@@ -3,5 +3,3 @@ color = input("what is the name of your favorite color?\n")
 word = input("what is a random word you always loved?\n")
 print ("The name of your Band is : " + color + " " + word )
 
-
-
