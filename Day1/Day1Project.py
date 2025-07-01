47aab45e9fe9d1fc7683e7a972118d228aa3051b
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-01 12:02:06 +0100

    Fix: Remove unnecessary commas and string concatenation

--- Day1/Day1Project.py
+++ Day1/Day1Project.py
@@ -1,7 +1,7 @@
 print("Welcome to My Band name generator!")
-color = input("what is the name of your favorite color?\n"),
-word = input("what is a random word you always loved?\n"),
-print ("The name of your Band is : " + color + " " + word + " " + "Rocks")
+color = input("what is the name of your favorite color?\n")
+word = input("what is a random word you always loved?\n")
+print ("The name of your Band is : " + color + " " + word )
 
 
 
