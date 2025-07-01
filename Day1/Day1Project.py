eb0b0e6e5922e746d5c4cc3c3b0c523a715fc24c
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-01 11:58:06 +0100

    Fix: Correct band name concatenation

--- Day1/Day1Project.py
+++ Day1/Day1Project.py
@@ -1,6 +1,6 @@
 print("Welcome to My Band name generator!")
 color = input("what is the name of your favorite color\n"),
 word = input("what is a random word you've always loved\n"),
-print("The name of your Band is: ")
+print("The name of your Band is:" color + word )
 
 
