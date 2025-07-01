0c31b0be149fff3604c9394255154af05d058313
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-01 11:56:06 +0100

    Fix: Remove trailing commas from input prompts

--- Day1/Day1Project.py
+++ Day1/Day1Project.py
@@ -1,5 +1,5 @@
 print("Welcome to My Band name generator!")
-color = input("what is the name of your favorite color")
-word= input("what is a random word you've always loved")
+color = input("what is the name of your favorite color"),
+word= input("what is a random word you've always loved"),
 
 
