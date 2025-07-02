5c8e38181eb1e9e228f6432344d8b0477ff1989d
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 10:38:03 +0100

    Fix: Calculate tip percentage correctly.

--- Day2/Day2 Project.py
+++ Day2/Day2 Project.py
@@ -3,17 +3,9 @@ total_bill = float(input("What was the total bill: \n"))
 tip = int(input("How much tip would you like to give?, $10, $12 or $15? \n"))
 if tip == 10:
     tip = 0.1
-elif tip == 12:
-    tip = 0.12
-elif tip == 15:
-    tip = 0.15
 else:
     tip = tip/100
-
 people = int(input("How many people are splitting the bill? \n="))
 bill = (total_bill + (tip * total_bill)) / (people)
 print(f"Each person is to pay ${bill:.2f}")
-
-
-
 
