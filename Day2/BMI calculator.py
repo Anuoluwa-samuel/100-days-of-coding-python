4f55150474dd9c0c95f98038c4514ce05f628caa
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 09:44:02 +0100

    feat: Prompt for weight and height inputs

--- Day2/BMI calculator.py
+++ Day2/BMI calculator.py
@@ -1,4 +1,5 @@
 # The body mass index (BMI) is a measure used in medicine to see if someone is underweight or overweight. 
 # bmi is equal to the person's weight divided by the person's height squared.
 
-Weight
+Weight = float(input("Enter your weight in KG\n"))
+Height = float(input(""))
