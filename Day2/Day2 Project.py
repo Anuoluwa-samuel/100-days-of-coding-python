23433e035d567d0e7445abc3c344a753c18f707b
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 10:14:03 +0100

    Fix: Calculate tip amount using user input

--- Day2/Day2 Project.py
+++ Day2/Day2 Project.py
@@ -2,12 +2,10 @@ print("Welcome to the tip calculator")
 bill = input("What was the total bill: \n")
 tip = input("How much tip would you like to give?, $10, $12 or $20? \n")
 if tip == 10 :
-    tip = 0.1,
-    print(f"Tip amount is ${tip * float(bill)}")
-elif tip == 12:
-    tip = 0.12,
-elif tip == 15:
-    tip = 0.15,
+    tip = 0.1
+
+print(f"Tip amount is ${tip * float(bill)}")
+
 
 
 
