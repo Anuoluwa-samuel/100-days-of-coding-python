1d28a9e6e73a70df8d06e9e2ae26616ad7cfb0f6
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 15:42:59 +0100

    Refactor: Simplify tip calculation in tip calculator

--- Day2/Day2 Project.py
+++ Day2/Day2 Project.py
@@ -1,10 +1,6 @@
 print("Welcome to the tip calculator")
 total_bill = float(input("What was the total bill: \n"))
-tip = int(input("How much tip would you like to give?, $10, $12 or $15? \n"))
-if tip == 10:
-    tip = 0.1
-else:
-    tip = tip/100
+tip = int(input("How much tip would you like to give?, $10, $12 or $15? \n")) / 100
 people = int(input("How many people are splitting the bill? \n="))
 bill = (total_bill + (tip * total_bill)) / (people)
 print(f"Each person is to pay ${bill:.2f}")
