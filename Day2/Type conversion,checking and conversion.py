2b912ea6cc27e3709f2a87f870861aa753535e16
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 08:48:44 +0100

    Refactor: Add comment explaining type conversion in print statement.

--- Day2/Type conversion,checking and conversion.py
+++ Day2/Type conversion,checking and conversion.py
@@ -1,3 +1,3 @@
 name = input("what is your name\n") 
 length = len(name) 
-print("Numbers of letter in your name: " + str(length))
+print("Numbers of letter in your name: " + str(length)) #the purpose of the str was to convert the data type of the variable length to string so that it can be concatenated with the string "Numbers of letter in
