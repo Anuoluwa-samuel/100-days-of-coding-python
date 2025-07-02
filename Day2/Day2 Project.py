e2e6b1d0a23936188906f91ca87b6650fb462d64
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 10:09:03 +0100

    Fix: Correct typo and add missing 'else' condition in tip calculation

--- Day2/Day2 Project.py
+++ Day2/Day2 Project.py
@@ -1,9 +1,11 @@
 print("Welcome to the tip calculator")
 bill = input("What was the total bill: \n")
-tip = input("How much tip would you like to give?, $10, $12, $20 \n")
+tip = input("How much tip would you like to give?, $10, $12 or $20? \n")
 if tip == 10 :
     tip = 0.1,
 elif tip == 12:
     tip = 0.12,
 elif tip == 15:
     tip = 0.15,
+else:
+    
