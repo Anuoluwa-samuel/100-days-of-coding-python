a9b7bd076baa8e27d588ddb210a90015d41c769e
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 16:18:56 +0100

    feat: Implement basic pizza order input

---
+++ Day3/pizza_exercise.py
@@ -0,0 +1,6 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+l
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
