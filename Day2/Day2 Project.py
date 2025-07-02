eae9e6b672b944aa2d8248866d4efe91224b229b
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 10:07:02 +0100

    Fix: Corrected tip percentage assignment in calculator.

--- Day2/Day2 Project.py
+++ Day2/Day2 Project.py
@@ -2,5 +2,8 @@ print("Welcome to the tip calculator")
 bill = input("What was the total bill: \n")
 tip = input("How much tip would you like to give: \n")
 if tip == 10 :
-    tip = 0.1
-    elif tip <= 12:
+    tip = 0.1,
+elif tip == 12:
+    tip = 0.12,
+elif tip == 15:
+    tip = 0.15
