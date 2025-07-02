405c8413ab036fecaee48283fe1b679a426f2ef2
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 09:43:02 +0100

    feat: Add initial BMI calculator Python file

---
+++ Day2/BMI calculator.py
@@ -0,0 +1,4 @@
+# The body mass index (BMI) is a measure used in medicine to see if someone is underweight or overweight. 
+# bmi is equal to the person's weight divided by the person's height squared.
+
+Weight
