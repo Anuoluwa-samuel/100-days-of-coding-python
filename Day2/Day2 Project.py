ca6cdc9a086ae0ebd4c50467b25b8e7aa432b6d0
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 10:17:02 +0100

    Fix: Corrected tip options to $10, $12, or $15

--- Day2/Day2 Project.py
+++ Day2/Day2 Project.py
@@ -1,13 +1,13 @@
 print("Welcome to the tip calculator")
 bill = float(input("What was the total bill: \n"))
-tip = int(input("How much tip would you like to give?, $10, $12 or $20? \n"))
+tip = int(input("How much tip would you like to give?, $10, $12 or $15? \n"))
 if tip == 10 :
     tip = 0.1
 elif tip == 12:
     tip = 0.12
 elif tip == 15:
     tip = 0.15
-    
+
 print(f"Tip amount is ${tip * bill}")
 
 
