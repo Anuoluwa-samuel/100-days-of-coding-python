cff0baf9d441e668de24ff3938d4a4729948efca
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 16:11:56 +0100

    Fix: Improve age-based pricing in rollercoaster ride

--- Day3/Nested_statements.py
+++ Day3/Nested_statements.py
@@ -5,11 +5,13 @@ if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("To know your ride fee, declare your age: \n"))
     if age <= 13:
-        print("To take the ride you are to pay 20 naira")
+        print("To take the ride You are to pay 20 naira")
     elif age <= 19:
-        print("To take the ride you are to pay 30 naira")
+        print("To take the ride You are to pay 30 naira")
+    elif age <= 30:
+        print("Oh ye adult, Your ride fee is 50 naira")
     else:
-        print("Oh ye adult, your ride fee is 50 naira")
+        print("Ancient one, You are to pay 100 naira")
 
 else:
     print("Get thee behind me, you dwarf")
