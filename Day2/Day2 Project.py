c4ced02054a064c74be4cf57115afc0483593d83
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-02 10:06:02 +0100

    Fix: Corrected tip calculation and added conditional for 12% tip

--- Day2/Day2 Project.py
+++ Day2/Day2 Project.py
@@ -2,4 +2,5 @@ print("Welcome to the tip calculator")
 bill = input("What was the total bill: \n")
 tip = input("How much tip would you like to give: \n")
 if tip == 10 :
-    
+    tip = 0.1
+    elif tip <= 12:
