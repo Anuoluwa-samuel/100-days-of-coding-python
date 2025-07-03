976ab8efcfd994634f8abcc14687a50432ab2666
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-03 11:32:30 +0100

    feat: Remove Day3 exercises

--- Day3/Modulo_exercise.py
+++
@@ -1,5 +0,0 @@
-number = int(input("Enter a Positive number: \n"))
-if number % 2 == 0:
-    print(f"The given number {number} is an even number. ")
-else:
-    print(f"The given number {number} is an odd number. ")
