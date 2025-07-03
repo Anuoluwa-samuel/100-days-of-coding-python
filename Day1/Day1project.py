468e6a42991288d26722bae566dba9f5e8c65768
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-03 11:30:31 +0100

    feat: Add instructions to the band name generator

--- Day1/Day1project.py
+++ Day1/Day1project.py
@@ -1,4 +1,5 @@
 print("Welcome to My Band name generator!")
+print("Please enter the following details to generate your band name:")
 color = input("what is the name of your favorite color?\n")
 word = input("what is a random word you always loved?\n")
 print ("The name of your Band is : " + color + " " + word )
