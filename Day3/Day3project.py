5799a1e8bd881b9349b107ac1d425aa37d24e12f
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-10 14:04:18 +0100

    feat: Add lake encounter to treasure island game

--- Day3/Day3project.py
+++ Day3/Day3project.py
@@ -26,5 +26,7 @@ print("You are at a cross road. where do you want to go?")
 road = input("Type 'left' to go left or 'right' to go right\n")
 if road == "left":
     print("You have come to a lake. There is an island in the middle of the lake")
+    lake = input("Type 'wait' to wait for a boat or 'swim' to swim across\n")
+    if lake == "wait"
 else:
     print("You fell into a hole. GAME OVER")
