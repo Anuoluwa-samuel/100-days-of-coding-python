64b4b9f1b10b78cc10f78ae31a73d07647561fb2
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-10 13:44:19 +0100

    Fix: Improve user experience with more informative rollercoaster ride fee messages.

--- Day3/Nested_statements.py
+++ Day3/Nested_statements.py
@@ -2,16 +2,16 @@ print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? \n"))
 
 if height >= 120:
-    print("You can ride the rollercoaster")
+    print("You are allowed to ride the rollercoaster!")
     age = int(input("To know your ride fee, declare your age: \n"))
     if age <= 13:
-        print("To take the ride You are to pay 20 naira")
+        print("To take the ride You need to pay 20 naira")
     elif age <= 19:
-        print("To take the ride You are to pay 30 naira")
+        print("To take the ride You need  to pay 30 naira")
     elif age <= 30:
         print("Oh ye adult, Your ride fee is 50 naira")
     else:
-        print("Ancient one, You are to pay 100 naira")
+        print("Ancient one, You need to pay 100 naira")
 
 else:
     print("Get thee behind me, you dwarf")
