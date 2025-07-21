4d0f3618646e07859f2acd7c011332ed10e2707c
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-21 10:40:40 +0100

    feat: Implement rock, paper, scissors game

--- Day4/Day4project.py
+++ Day4/Day4project.py
@@ -1,8 +1,63 @@
-if a == "rock" and b == "scissors":
-    print("a wins")
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-if a == "scissors" and b == "paper":
-    print("a wins")
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-if use= "paper" and b == "rock":
-    print("a wins")
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+user=int(input("what do you choose? Type 0 for rock, 1 for paper or 2 for scissors.\n"))
+if user == 0:
+    print("You chose Rock:")
+    print(rock)
+elif user == 1:
+    print("You chose Paper:")
+    print(paper)
+elif user == 2:
+    print("You chose Scissors:")
+    print(scissors)
+else:
+    print("Invalid input! You lose.")
+    exit()
+
+print(user)
+
+print("Computer chose")
+computer_choice = random.randint(0,2)
+if computer_choice == 0:
+    print(rock)
+elif computer_choice == 1:
+    print(paper)
+elif computer_choice == 2:
+    print(scissors)
+
+if user == computer_choice:
+    print("It's a draw!")
+elif user == 0 and computer_choice == 2:
+    print("You win!")
+elif user == 1 and computer_choice == 0:
+    print("You win!")
+elif user == 2 and computer_choice == 1:
+    print("You win!")
+else:
+    print("You lose!")
