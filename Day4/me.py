3743924eeeb9b77ba0bee824484d2241f371444f
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-21 09:25:39 +0100

    Fix: Print correct vegetable from dirty_dozen list

--- Day4/me.py
+++ Day4/me.py
@@ -2,4 +2,4 @@ fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries
 vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
 
 dirty_dozen = [fruits, vegetables]
-print(dirty_dozen[1][2])
+print(dirty_dozen[1][3])
