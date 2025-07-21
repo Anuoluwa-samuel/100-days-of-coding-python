6b87057742113f5a606fa509000a17202e3689c9
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-21 09:35:40 +0100

    Fix: Prompt user before evaluating input in rock paper scissors

--- Day4/Day4project.py
+++ Day4/Day4project.py
@@ -1,3 +1,6 @@
 import random 
 
-print(input("what do you choose? Type 0 for rock, 1 for paper or 2 for scissors.\n"))
+print("what do you choose? Type 0 for rock, 1 for paper or 2 for scissors.")
+game = input(">").lower()
+
+if game == 1 
