ae7d667f75f3b2bafaad5dbc5fd26b9cc6001bf7
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-21 10:27:39 +0100

    Refactor: Remove unused win/loss logic.

--- Day4/Day4project.py
+++ Day4/Day4project.py
@@ -1,9 +1,2 @@
-if (user == "scissors" and computer_choice == "rock") or \
-   (user == "paper" and computer_choice == "scissors") or \
-   (user == "rock" and computer_choice == "paper"):
-    print("computer_choice wins")
 
-if (user == "rock" and computer_choice == "scissors") or \
-   (user == "scissors" and computer_choice == "paper") or \
-   (user == "paper" and computer_choice == "rock"):
-    print("You wins")
+
