6365d316ea2c1a5e14ca8317ac0546522abb8169
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-21 09:37:39 +0100

    Fix: Display paper ASCII art when user selects '1'

--- Day4/Day4project.py
+++ Day4/Day4project.py
@@ -4,4 +4,12 @@ print("what do you choose? Type 0 for rock, 1 for paper or 2 for scissors.")
 game = input(">").lower()
 
 if game == 1:
-    print
+    print(r'''
+          _______
+      ---'   ____)
+            (_____)
+            (_____)
+    VK      (____)
+      ---.__(___)
+
+          ''')
