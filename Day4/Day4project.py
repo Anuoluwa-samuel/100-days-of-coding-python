0464236e61374921bfc598090e6dfcd126c851e1
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-21 09:31:39 +0100

    feat: Import random module for game logic

--- Day4/Day4project.py
+++ Day4/Day4project.py
@@ -1,1 +1,4 @@
-print(input"what do you choose? Type 0 for rock, 1 for paper or 2 for scissors.")
+import random 
+
+
+print(input("what do you choose? Type 0 for rock, 1 for paper or 2 for scissors.\n"))
